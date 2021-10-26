ebda38b927ec95e0d9e6032dc5f425014b0e44a3
Ahmad Hamze <ahmadhamze@yahoo.com>
2021-10-26 17:00:36 +0300

    Initialize a basic selenium code The code is able to lauch a browser and navigate to 10fastfingers.com

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # This is a project intending to test the typing speed of Selenium
+
+You need to install the latest web driver [Chrome web driver](https://sites.google.com/chromium.org/driver/)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    print("Hello World!")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
-if __name__ == "__main__":
-    main()
+PATH = "/home/ahmad/Templates/chrome-driver/chromedriver"
+serv = Service(PATH)
+driver = webdriver.Chrome(service=serv)
+driver.get("https://10fastfingers.com/")
+
+time.sleep(3)
+
+driver.quit()
