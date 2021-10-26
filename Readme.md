bbe1a675f4bb5eac546295a11a267af12a076fbc
Ahmad Hamze <ahmadhamze@yahoo.com>
2021-10-27 00:12:41 +0300

    Grab the needed words All the words that have to be typed are collected into a list, this was done using the request-html library. There is a line that I don't understand in the code (c.f. Readme)

--- Readme.md
+++ Readme.md
@@ -5,3 +5,13 @@ You need to install the latest web driver [Chrome web driver](https://sites.goog
 ## Trying to grab all the words to type
 
 If you try to grab all the words to type, you will be able to grab around 20 of them, the rest of the words will be empty strings!
+
+## Solution
+
+Grabbing all the words is successful! I used the requests-html library to parse the selenium object "driver.page_source".
+
+There is a something that I don't understand yet, when this line is removed
+
+`wordsContainer.find_elements(By.TAG_NAME, "span")`
+
+the words cannot be reached (we have an empty div), the above code seems useless at the first glance, but it's crucial to getting the words.
--- main.py
+++ main.py
@@ -3,26 +3,24 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from requests_html import HTML
 
 PATH = "/home/ahmad/Templates/chrome-driver/chromedriver"
 serv = Service(PATH)
 driver = webdriver.Chrome(service=serv)
-driver.get("https://10fastfingers.com/")
+driver.get("https://10fastfingers.com/typing-test/english")
 try:
     
-    startButton = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "typing-test"))
-    )
-    startButton.click()
     wordsContainer = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "row1"))
     )
     driver.implicitly_wait(10)
-    spans = wordsContainer.find_elements(By.TAG_NAME, "span")
+    wordsContainer.find_elements(By.TAG_NAME, "span")
     
-    print(f"number of words: {len(spans)}")
-    for span in spans:
-        print(span.text)
+    html = HTML(html=driver.page_source)
+    match = html.find("#row1", first=True)
+    words = match.text.split(" ")
+    print(words)
 except Exception:
     print("An error occured!")
 
