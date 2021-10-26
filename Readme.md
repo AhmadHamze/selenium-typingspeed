db99de3a46660acb7b8bdb7ea2c0575396829e17
Ahmad Hamze <ahmadhamze@yahoo.com>
2021-10-26 20:33:34 +0300

    Failed to grab all words Despite having almost the correct number of words, only around 20 are actual words, the rest are empty strings

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 # This is a project intending to test the typing speed of Selenium
 
 You need to install the latest web driver [Chrome web driver](https://sites.google.com/chromium.org/driver/)
+
+## Trying to grab all the words to type
+
+If you try to grab all the words to type, you will be able to grab around 20 of them, the rest of the words will be empty strings!
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 PATH = "/home/ahmad/Templates/chrome-driver/chromedriver"
 serv = Service(PATH)
 driver = webdriver.Chrome(service=serv)
 driver.get("https://10fastfingers.com/")
-
-time.sleep(3)
+try:
+    
+    startButton = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "typing-test"))
+    )
+    startButton.click()
+    wordsContainer = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "row1"))
+    )
+    driver.implicitly_wait(10)
+    spans = wordsContainer.find_elements(By.TAG_NAME, "span")
+    
+    print(f"number of words: {len(spans)}")
+    for span in spans:
+        print(span.text)
+except Exception:
+    print("An error occured!")
 
 driver.quit()
